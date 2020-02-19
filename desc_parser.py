e4ba2dc031a2e098e70159148c67bd20ab8de3ff
mcglincy <mattmcglincy@gmail.com>
2020-02-19 10:33:23 -0600

    DRY up parsing routines.

--- desc_parser.py
+++ desc_parser.py
@@ -1,20 +1,6 @@
 #!/usr/bin/python3
 
-import json
 from desc import *
+from parser import *
 
-
-filename = './datafiles/desc.mon'
-with open(filename, 'rb') as f:
-  descs = []
-  record_index = 1  # 1-based
-  # 1260 records
-  while has_more_data(f):  
-    desc = read_desc(f)
-    desc["id"] = record_index
-    record_index = record_index + 1
-    descs.append(desc)
-  print(json.dumps(descs, indent = 2, sort_keys=False))
-
-
-
+parse_file('./datafiles/desc.mon', read_desc)
--- roomdesc_parser.py
+++ roomdesc_parser.py
@@ -1,20 +1,6 @@
 #!/usr/bin/python3
 
-import json
+from parser import *
 from roomdesc import *
 
-
-filename = './datafiles/roomdesc.mon'
-with open(filename, 'rb') as f:
-  roomdescs = []
-  record_index = 1  # 1-based
-  # 752 records
-  while has_more_data(f):
-    roomdesc = read_roomdesc(f)
-    roomdesc["id"] = record_index
-    record_index = record_index + 1
-    roomdescs.append(roomdesc)
-  print(json.dumps(roomdescs, indent = 2, sort_keys=False))
-
-
-
+parse_file('./datafiles/roomdesc.mon', read_roomdesc)
